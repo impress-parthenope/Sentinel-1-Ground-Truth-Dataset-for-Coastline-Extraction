931fa3d49680ccf85076450d3a8cb3b6a4530e47
impress-parthenope <85936968+impress-parthenope@users.noreply.github.com>
2026-05-06 12:35:44 +0200

    Add files via upload

---
+++ run_otb_eea.bat
@@ -0,0 +1,3 @@
+@echo off 
+
+<OTB_PATH>\OTB-8.1.1-Win64\bin\\otbcli_Rasterization.bat -in <EEA_PATH>\EEA_Coastline_Polyline_Shape\Europe_coastline_raw_rev2017.shp -out <Out_PATH>\Masks\SARVH_mask2.tif -mode.binary.foreground 1.0 -im <SAR_TIFF_PATH>\SARVH.tif
