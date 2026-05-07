2490411cf42c685062db0fdbc5f9dbbc40d2f00d
impress-parthenope <85936968+impress-parthenope@users.noreply.github.com>
2026-05-07 11:23:38 +0200

    Update run_otb_euhydro.bat

--- run_otb_euhydro.bat
+++ run_otb_euhydro.bat
@@ -1,3 +1,3 @@
 @echo off 
 
-<OTB_PATH>\OTB-8.1.1-Win64\bin\\otbcli_Rasterization.bat -in <EUH_PATH>\EUHYDRO_Coastline_EEA39_v012\Shapefile\EUHYDRO_Coastline_EEA39_v012.shp -out <Out_PATH>\Masks\SARVH_mask.tif -mode.binary.foreground 1.0 -im <SAR_TIFF_PATH>\SARVH.tif 
+<OTB_PATH>\OTB-8.1.1-Win64\bin\\otbcli_Rasterization.bat -in <EUH_PATH>\EUHYDRO_Coastline_EEA39_v012\Shapefile\EUHYDRO_Coastline_EEA39_v012.shp -out <Out_PATH>\Masks\<filename>_mask.tif -mode.binary.foreground 1.0 -im <SAR_TIFF_PATH>\<filename>.tif 
